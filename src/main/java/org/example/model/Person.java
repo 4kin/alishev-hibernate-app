81285075aeb5ea8af2cd3f786e522765f7dcf7cc
4kin.nikita <4kin.nikita@gmail.com>
2023-02-02 23:40:41 +0300

    48 Первое приложение с Hibernate.mkv

--- src/main/java/org/example/model/Person.java
+++ src/main/java/org/example/model/Person.java
@@ -1,7 +1,55 @@
 package org.example.model;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
 @Entity
+@Table(name = "Person")
 public class Person {
+
+    @Id
+    @Column(name ="id")
+    private  int id;
+    @Column(name = "name", length = 100)
+    private String name;
+    @Column(name = "age")
+    private Integer age;
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public Person() {
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", age=" + age +
+                '}';
+    }
 }
